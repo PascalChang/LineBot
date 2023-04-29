8e3d2c0853d0c9473ff6d997569959d6c8e2b3a3
Pascal <pascalchang09@gmail.com>
2023-04-29 22:43:32 +0800

    update

--- app.js
+++ app.js
@@ -63,5 +63,5 @@ async function handleEvent(event) {
 // listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`listening on ${port}`);
+  console.log(`listening on ${port}--`);
 });
